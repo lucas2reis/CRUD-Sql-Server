05f4779571144e4a357133278c06688e790afed8
Lucas dos Reis <lucasreis1413@gmail.com>
2023-01-11 12:30:55 -0300

    retirando atribuições desnecessárias

--- CRUDSQL/Models/ErrorViewModel.cs
+++ CRUDSQL/Models/ErrorViewModel.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace CRUDSQL.Models
 {
     public class ErrorViewModel
--- CRUDSQL/Startup.cs
+++ CRUDSQL/Startup.cs
@@ -1,16 +1,8 @@
-using CRUDSQL.Models;
-using FluentAssertions.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using CRUDSQL.Data;
 
